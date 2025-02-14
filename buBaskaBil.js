0d1b64207258934d708a20b6266a46f99ec7ebba
mwgules <mwgules@gmail.com>
2025-02-14 16:03:14 +0300

    diger biligisayardan ekleme yapildi.

---
+++ buBaskaBil.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const buBaskaBil = () => {
+    return (
+        <View>
+        <Text>buBaskaBil</Text>
+        </View>
+    );
+};
+
+export default buBaskaBil;
